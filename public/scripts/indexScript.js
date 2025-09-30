3c049d71dfed2039b7a7ad781fcc124417f018f0
Aysaes <escalada.278464@legazpi.sti.edu.ph>
2025-09-30 19:21:18 +0800

    fixed inbdex js button event

--- public/scripts/indexScript.js
+++ public/scripts/indexScript.js
@@ -1,7 +1,7 @@
 // Buttons
 
 document.getElementById("loginBtn").addEventListener("click", () => {
-    window.location.href = "/auth/google";
+    window.location.href = "https://greenlinklolasayong.site/auth/google/callback";
 });
 
 
