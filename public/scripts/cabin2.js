cec69af1b168b333fcd9f521a123d3584ba05674
kebeanito <projectpurposes1220@gmail.com>
2025-10-01 16:57:13 +0800

    fixed script navigations

--- public/scripts/cabin2.js
+++ public/scripts/cabin2.js
@@ -84,3 +84,73 @@ function openPaymentModal() {
   document.getElementById("alertModal").classList.add("hidden");
     }
 
+         //Pages  
+
+  //Room Reservation Page
+  document.addEventListener("DOMContentLoaded", () => {
+  
+  const btn = document.getElementById("roomReser");
+
+
+  btn.addEventListener("click", () => {
+    window.location.href = "../pages/RoomReser.html"; // go to another page
+  });
+});
+
+//Event Reservation Page
+
+  document.addEventListener("DOMContentLoaded", () => {
+  
+  const btn = document.getElementById("eventReser");
+
+
+  btn.addEventListener("click", () => {
+    window.location.href = "../pages/EventReser.html"; // go to another page
+  });
+});
+
+
+//Food Order Page
+
+  document.addEventListener("DOMContentLoaded", () => {
+  
+  const btn = document.getElementById("foodOrder");
+
+
+  btn.addEventListener("click", () => {
+    window.location.href = "../pages/FarmOrders.html"; // go to another page
+  });
+});
+
+//Farm Order Page
+
+  document.addEventListener("DOMContentLoaded", () => {
+  
+  const btn = document.getElementById("farmOrder");
+
+
+  btn.addEventListener("click", () => {
+    window.location.href = "../pages/FarmOrders.html"; // go to another page
+  });
+});
+
+    // --- Modal Functions ---
+function openReserModal() {
+  document.getElementById('reservationModal').classList.remove('hidden');
+  document.body.classList.add("overflow-hidden"); // disable scroll
+}
+
+function closeReserModal() {
+  document.getElementById('reservationModal').classList.add('hidden');
+  document.body.classList.remove("overflow-hidden"); // re-enable scroll
+}
+
+function openOrderModal() {
+  document.getElementById('orderModal').classList.remove('hidden');
+  document.body.classList.add("overflow-hidden"); // disable scroll
+}
+
+function closeOrderModal() {
+  document.getElementById('orderModal').classList.add('hidden');
+  document.body.classList.remove("overflow-hidden"); // re-enable scroll
+}
--- public/scripts/twinRoom.js
+++ public/scripts/twinRoom.js
@@ -103,3 +103,52 @@ function closeOrderModal() {
   document.getElementById('orderModal').classList.add('hidden');
   document.body.classList.remove("overflow-hidden"); // re-enable scroll
 }
+      //Pages  
+
+  //Room Reservation Page
+  document.addEventListener("DOMContentLoaded", () => {
+  
+  const btn = document.getElementById("roomReser");
+
+
+  btn.addEventListener("click", () => {
+    window.location.href = "../pages/RoomReser.html"; // go to another page
+  });
+});
+
+//Event Reservation Page
+
+  document.addEventListener("DOMContentLoaded", () => {
+  
+  const btn = document.getElementById("eventReser");
+
+
+  btn.addEventListener("click", () => {
+    window.location.href = "../pages/EventReser.html"; // go to another page
+  });
+});
+
+
+//Food Order Page
+
+  document.addEventListener("DOMContentLoaded", () => {
+  
+  const btn = document.getElementById("foodOrder");
+
+
+  btn.addEventListener("click", () => {
+    window.location.href = "../pages/FarmOrders.html"; // go to another page
+  });
+});
+
+//Farm Order Page
+
+  document.addEventListener("DOMContentLoaded", () => {
+  
+  const btn = document.getElementById("farmOrder");
+
+
+  btn.addEventListener("click", () => {
+    window.location.href = "../pages/FarmOrders.html"; // go to another page
+  });
+});
