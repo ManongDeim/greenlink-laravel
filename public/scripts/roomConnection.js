4017ef6952a9e74c97e2fde84a1c6e2516e56939
Deim Troi <deimdgreat@gmail.com>
2025-10-22 22:43:23 +0800

    Deleted unused files

--- public/scripts/roomConnection.js
+++ public/scripts/roomConnection.js
@@ -251,7 +251,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
       // Book Now → redirect
       modal.querySelector(".book-now-btn").addEventListener("click", () => {
-        window.location.href = `../pages/SquareRoom.html?id=${room.id}`;
+        window.location.href = `../pages/BookingPage.html?id=${room.id}`;
       });
 
       container.appendChild(card);
