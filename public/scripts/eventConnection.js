9580f14f5b97e1568221e13acb80d68555ae1c6a
John-code69 <arriegajohnangel@gmail.com>
2025-10-22 19:21:45 +0800

    fix terms and condition

--- public/scripts/eventConnection.js
+++ public/scripts/eventConnection.js
@@ -196,6 +196,7 @@ function goBack() {
 }
 
 
+
 // Validation 
 const fullnameInput = document.getElementById("fullname");
 const nameError = document.getElementById("nameError");
@@ -278,3 +279,22 @@ if (eventTypeInput.value === '') {
     openConfirmationModal();
   }
 }
+
+function openTermsModal() {
+      document.getElementById('termsModal').classList.remove('hidden');
+      document.getElementById('termsModal').classList.add('flex');
+    }
+    function closeTermsModal() {
+      document.getElementById('termsModal').classList.add('hidden');
+    }
+    function handleReviewClick() {
+      const checkbox = document.getElementById('termsCheckbox');
+      if (!checkbox.checked) {
+        document.getElementById('alertMessage').textContent = 'Please agree to the Terms and Conditions before proceeding.';
+        document.getElementById('alertModal').classList.remove('hidden');
+        return;
+      }
+      openPaymentModal();
+    }
+
+
