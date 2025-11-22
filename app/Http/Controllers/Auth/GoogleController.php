eed867d1f8fd410e734980a1ea61ee5b92feadba
Deim Troi <deimdgreat@gmail.com>
2025-11-22 22:11:13 +0800

    asd

--- app/Http/Controllers/Auth/GoogleController.php
+++ app/Http/Controllers/Auth/GoogleController.php
@@ -38,6 +38,7 @@ class GoogleController extends Controller
         ['user_id' => $user->id],
         [
             'email'  => $googleUserData->getEmail(),
+            'name'=> $googleUserData->getName(),
             'avatar' => $googleUserData->getAvatar(),
             'role'   => 'customer' // default role
         ]
