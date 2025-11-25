2d0ef8568bd57540f64c8405f1410129ab2dc5e4
kebeanito <projectpurposes1220@gmail.com>
2025-11-26 06:09:47 +0800

    gfd

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,6 @@ use App\Http\Controllers\Api\ReviewController;
 use App\Http\Controllers\Api\ApprovalController;
 use App\Http\Controllers\Api\HomePageController;
 use \App\Http\Controllers\Api\PaymongoWebhookController;
-use App\Http\Controllers\Api\NotificationController;
 use App\Models\EventAdminModel;
 use App\Models\RoomSeederModel;
 use App\Models\GoogleUser;
@@ -194,6 +193,3 @@ Route::delete('/{id}', [HomePageController::class, 'destroy']);
 //Paymongo
 
 Route::post('/webhook/paymongo', [PaymongoWebhookController::class, 'handleWebhook']);
-
-Route::middleware('auth:sanctum')
-    ->get('/notifications-counts', [NotificationController::class, 'getCounts']);
